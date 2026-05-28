d00fdee26c783a7514edd0c5b2e6d421ae57ebb7
Saideepak2004 <142832713+Saideepak2004@users.noreply.github.com>
2026-05-28 15:20:34 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/newport/' // Enforce relative asset paths for seamless GitHub Pages static hosting
+})
