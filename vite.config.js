f0a819454d11de3865086e951aff41fdd9ee2689
Saideepak2004 <142832713+Saideepak2004@users.noreply.github.com>
2026-05-28 15:25:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/newport/' // Enforce relative asset paths for seamless GitHub Pages static hosting
+  base: '/newportf/' // Enforce relative asset paths for seamless GitHub Pages static hosting
 })
